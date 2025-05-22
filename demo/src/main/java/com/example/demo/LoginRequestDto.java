3a832b9ddfc2aed8f3cf1c4352d623d837292ed7
jjm <hy08155@daum.net>
2025-05-22 09:59:23 +0900

    20250522

--- demo/src/main/java/com/example/demo/LoginRequestDto.java
+++ demo/src/main/java/com/example/demo/LoginRequestDto.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
+import lombok.Getter;
+
+@Getter
 public class LoginRequestDto {
     private String userId;
     private String password;
-
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 }
